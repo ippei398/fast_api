7c527f1a1d772f8b01d2a3309c97b320b3bccb22
IppeiNagao <ippei398@gmail.com>
2023-11-26 19:53:27 +0900

    fix

--- backend/main.py
+++ backend/main.py
@@ -13,13 +13,13 @@ app.include_router(route_auth.router)
 # アクセスを許可するURL
 origins = ['http://localhost:3000']
 # CORSの設定
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=['*'],
+#     allow_headers=['*']
+# )
 
 
 @CsrfProtect.load_config
